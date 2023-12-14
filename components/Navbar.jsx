6771a82cc7fb959129090f460bdfcfbc29b59a2e
SanketShendge21 <sanketshendge21052002@gmail.com>
2023-12-14 20:08:53 +0530

    Created Navbar and Footer and Added some content in homepage

---
+++ components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { CiShoppingCart } from "react-icons/ci";
+const Navbar = () => {
+  return (
+    <div className='flex flex-col md:flex-row md:justify-start justify-center items-center py-2'>
+        <div className="logo mx-5">
+            <Image src="/logo.png" alt="Error" width={200} height={40}/>
+        </div>
+        <div className="nav">
+            <ul className='flex items-center space-x-2 font-bold md:text-xl'>
+                <Link href={'/'}><li>Hoodies</li></Link>
+                <Link href={'/'}><li>Tshirts</li></Link>
+                <Link href={'/'}><li>Stickers</li></Link>
+                <Link href={'/'}><li>Mugs</li></Link>
+            </ul>
+        </div>
+        <div className="cart absolute right-0 top-4 mx-5">
+            <CiShoppingCart className='text-xl md:text-3xl'/>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
