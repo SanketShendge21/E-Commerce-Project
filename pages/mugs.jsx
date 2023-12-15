eff5e2d3929c7ef88a0cb859d88b793f220d773d
SanketShendge21 <sanketshendge21052002@gmail.com>
2023-12-15 13:20:31 +0530

    Designed more products pages

--- pages/mugs.jsx
+++ pages/mugs.jsx
@@ -1,9 +1,106 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
 const Mugs = () => {
-  return (
-    <div>Mugs</div>
-  )
-}
+	return (
+		<div>
+			<section className="text-gray-600 body-font">
+				<div className="container px-5 py-24 mx-auto">
+					<div className="flex flex-wrap -m-4">
+						<div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
+							<Link className="block relative rounded overflow-hidden" href={"/product/threadsunveiled"}>
+								<img alt="ecommerce" className="m-auto block" src="https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/71beHOHZKfL.__AC_SX300_SY300_QL70_FMwebp_.jpg" />
+								<div className="mt-4 text-center">
+									<h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRTS</h3>
+									<h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+									<p className="mt-1">₹499</p>
+									<p className="mt-1">S, M, L, XL, XXL</p>
+								</div>
+							</Link>
+						</div>
+						<div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
+							<Link className="block relative rounded overflow-hidden" href={"/product/threadsunveiled"}>
+								<img alt="ecommerce" className="m-auto block" src="https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/71beHOHZKfL.__AC_SX300_SY300_QL70_FMwebp_.jpg" />
+								<div className="mt-4 text-center ">
+									<h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRTS</h3>
+									<h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
+									<p className="mt-1">₹499</p>
+									<p className="mt-1">S, M, L, XL, XXL</p>
+								</div>
+							</Link>
+						</div>
 
-export default Mugs
+						<div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
+							<Link className="block relative rounded overflow-hidden" href={"/product/threadsunveiled"}>
+								<img alt="ecommerce" className="m-auto block" src="https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/71beHOHZKfL.__AC_SX300_SY300_QL70_FMwebp_.jpg" />
+								<div className="mt-4 text-center">
+									<h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRTS</h3>
+									<h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
+									<p className="mt-1">₹499</p>
+									<p className="mt-1">S, M, L, XL, XXL</p>
+								</div>
+							</Link>
+						</div>
+						<div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
+							<Link className="block relative rounded overflow-hidden" href={"/product/threadsunveiled"}>
+								<img alt="ecommerce" className="m-auto block" src="https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/71beHOHZKfL.__AC_SX300_SY300_QL70_FMwebp_.jpg" />
+								<div className="mt-4 text-center">
+									<h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRTS</h3>
+									<h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
+									<p className="mt-1">₹499</p>
+									<p className="mt-1">S, M, L, XL, XXL</p>
+								</div>
+							</Link>
+						</div>
+						<div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
+							<Link className="block relative rounded overflow-hidden" href={"/product/threadsunveiled"}>
+								<img alt="ecommerce" className="m-auto block" src="https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/71beHOHZKfL.__AC_SX300_SY300_QL70_FMwebp_.jpg" />
+								<div className="mt-4 text-center">
+									<h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRTS</h3>
+									<h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+									<p className="mt-1">₹499</p>
+									<p className="mt-1">S, M, L, XL, XXL</p>
+								</div>
+							</Link>
+						</div>
+						<div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
+							<Link className="block relative rounded overflow-hidden" href={"/product/threadsunveiled"}>
+								<img alt="ecommerce" className="m-auto block" src="https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/71beHOHZKfL.__AC_SX300_SY300_QL70_FMwebp_.jpg" />
+								<div className="mt-4 text-center">
+									<h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRTS</h3>
+									<h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
+									<p className="mt-1">₹499</p>
+									<p className="mt-1">S, M, L, XL, XXL</p>
+								</div>
+							</Link>
+						</div>
+						<div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
+							<Link className="block relative rounded overflow-hidden" href={"/product/threadsunveiled"}>
+								<img alt="ecommerce" className="m-auto block" src="https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/71beHOHZKfL.__AC_SX300_SY300_QL70_FMwebp_.jpg" />
+								<div className="mt-4 text-center">
+									<h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRTS</h3>
+									<h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
+									<p className="mt-1">₹499</p>
+									<p className="mt-1">S, M, L, XL, XXL</p>
+								</div>
+							</Link>
+						</div>
+						<div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
+							<Link className="block relative rounded overflow-hidden" href={"/product/threadsunveiled"}>
+								<img alt="ecommerce" className="m-auto block" src="https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/71beHOHZKfL.__AC_SX300_SY300_QL70_FMwebp_.jpg" />
+								<div className="mt-4 text-center">
+									<h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRTS</h3>
+									<h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
+									<p className="mt-1">₹499</p>
+									<p className="mt-1">S, M, L, XL, XXL</p>
+								</div>
+							</Link>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+	);
+};
+
+export default Mugs;
--- pages/stickers.jsx
+++ pages/stickers.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
 import Link from "next/link";
+import React from "react";
 
-const Hoodies = () => {
-  return (
-    <div>
-      			<section className="text-gray-600 body-font">
+const Stickers = () => {
+	return (
+		<div>
+			<section className="text-gray-600 body-font">
 				<div className="container px-5 py-24 mx-auto">
 					<div className="flex flex-wrap -m-4">
 						<div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
 							<Link className="block relative rounded overflow-hidden" href={"/product/threadsunveiled"}>
-								<img alt="ecommerce" className="m-auto h-[30vh] md:h-[36vh] block" src="https://rukminim2.flixcart.com/image/832/832/xif0q/sweatshirt/a/c/d/l-ss-1379-smartees-original-imagtjbkxzcvyhms.jpeg?q=70" />
+								<img alt="ecommerce" className="m-auto h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/614ZxGEtrQL._SX300_SY300_QL70_FMwebp_.jpg" />
 								<div className="mt-4 text-center">
 									<h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRTS</h3>
 									<h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
@@ -20,7 +20,7 @@ const Hoodies = () => {
 						</div>
 						<div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
 							<Link className="block relative rounded overflow-hidden" href={"/product/threadsunveiled"}>
-								<img alt="ecommerce" className="m-auto h-[30vh] md:h-[36vh] block" src="https://rukminim2.flixcart.com/image/832/832/xif0q/sweatshirt/a/c/d/l-ss-1379-smartees-original-imagtjbkxzcvyhms.jpeg?q=70" />
+								<img alt="ecommerce" className="m-auto h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/614ZxGEtrQL._SX300_SY300_QL70_FMwebp_.jpg" />
 								<div className="mt-4 text-center ">
 									<h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRTS</h3>
 									<h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
@@ -32,7 +32,7 @@ const Hoodies = () => {
 
 						<div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
 							<Link className="block relative rounded overflow-hidden" href={"/product/threadsunveiled"}>
-								<img alt="ecommerce" className="m-auto h-[30vh] md:h-[36vh] block" src="https://rukminim2.flixcart.com/image/832/832/xif0q/sweatshirt/a/c/d/l-ss-1379-smartees-original-imagtjbkxzcvyhms.jpeg?q=70" />
+								<img alt="ecommerce" className="m-auto h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/614ZxGEtrQL._SX300_SY300_QL70_FMwebp_.jpg" />
 								<div className="mt-4 text-center">
 									<h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRTS</h3>
 									<h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
@@ -43,7 +43,7 @@ const Hoodies = () => {
 						</div>
 						<div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
 							<Link className="block relative rounded overflow-hidden" href={"/product/threadsunveiled"}>
-								<img alt="ecommerce" className="m-auto h-[30vh] md:h-[36vh] block" src="https://rukminim2.flixcart.com/image/832/832/xif0q/sweatshirt/a/c/d/l-ss-1379-smartees-original-imagtjbkxzcvyhms.jpeg?q=70" />
+								<img alt="ecommerce" className="m-auto h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/614ZxGEtrQL._SX300_SY300_QL70_FMwebp_.jpg" />
 								<div className="mt-4 text-center">
 									<h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRTS</h3>
 									<h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
@@ -54,7 +54,7 @@ const Hoodies = () => {
 						</div>
 						<div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
 							<Link className="block relative rounded overflow-hidden" href={"/product/threadsunveiled"}>
-								<img alt="ecommerce" className="m-auto h-[30vh] md:h-[36vh] block" src="https://rukminim2.flixcart.com/image/832/832/xif0q/sweatshirt/a/c/d/l-ss-1379-smartees-original-imagtjbkxzcvyhms.jpeg?q=70" />
+								<img alt="ecommerce" className="m-auto h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/614ZxGEtrQL._SX300_SY300_QL70_FMwebp_.jpg" />
 								<div className="mt-4 text-center">
 									<h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRTS</h3>
 									<h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
@@ -65,7 +65,7 @@ const Hoodies = () => {
 						</div>
 						<div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
 							<Link className="block relative rounded overflow-hidden" href={"/product/threadsunveiled"}>
-								<img alt="ecommerce" className="m-auto h-[30vh] md:h-[36vh] block" src="https://rukminim2.flixcart.com/image/832/832/xif0q/sweatshirt/a/c/d/l-ss-1379-smartees-original-imagtjbkxzcvyhms.jpeg?q=70" />
+								<img alt="ecommerce" className="m-auto h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/614ZxGEtrQL._SX300_SY300_QL70_FMwebp_.jpg" />
 								<div className="mt-4 text-center">
 									<h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRTS</h3>
 									<h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
@@ -76,7 +76,7 @@ const Hoodies = () => {
 						</div>
 						<div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
 							<Link className="block relative rounded overflow-hidden" href={"/product/threadsunveiled"}>
-								<img alt="ecommerce" className="m-auto h-[30vh] md:h-[36vh] block" src="https://rukminim2.flixcart.com/image/832/832/xif0q/sweatshirt/a/c/d/l-ss-1379-smartees-original-imagtjbkxzcvyhms.jpeg?q=70" />
+								<img alt="ecommerce" className="m-auto h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/614ZxGEtrQL._SX300_SY300_QL70_FMwebp_.jpg" />
 								<div className="mt-4 text-center">
 									<h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRTS</h3>
 									<h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
@@ -87,7 +87,7 @@ const Hoodies = () => {
 						</div>
 						<div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-5">
 							<Link className="block relative rounded overflow-hidden" href={"/product/threadsunveiled"}>
-								<img alt="ecommerce" className="m-auto h-[30vh] md:h-[36vh] block" src="https://rukminim2.flixcart.com/image/832/832/xif0q/sweatshirt/a/c/d/l-ss-1379-smartees-original-imagtjbkxzcvyhms.jpeg?q=70" />
+								<img alt="ecommerce" className="m-auto h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/614ZxGEtrQL._SX300_SY300_QL70_FMwebp_.jpg" />
 								<div className="mt-4 text-center">
 									<h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">TSHIRTS</h3>
 									<h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
@@ -99,8 +99,8 @@ const Hoodies = () => {
 					</div>
 				</div>
 			</section>
-    </div>
-  )
-}
+		</div>
+	);
+};
 
-export default Hoodies
+export default Stickers;
