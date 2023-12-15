c2dd6ec6612fd209adeec33306cd3bde038a375a
SanketShendge21 <sanketshendge21052002@gmail.com>
2023-12-15 12:48:01 +0530

    Fixed some styling

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,20 +4,20 @@ import Link from 'next/link';
 import { CiShoppingCart } from "react-icons/ci";
 const Navbar = () => {
   return (
-    <div className='flex flex-col md:flex-row md:justify-start justify-center items-center py-2'>
+    <div className='flex flex-col md:flex-row md:justify-start justify-center items-center py-2 shadow-md'>
         <div className="logo mx-5">
-            <Image src="/logo.png" alt="Error" width={200} height={40}/>
+            <Link href={'/'}><Image src="/logo.png" alt="Error" width={200} height={40}/></Link>
         </div>
         <div className="nav">
-            <ul className='flex items-center space-x-2 font-bold md:text-xl'>
-                <Link href={'/'}><li>Hoodies</li></Link>
-                <Link href={'/'}><li>Tshirts</li></Link>
-                <Link href={'/'}><li>Stickers</li></Link>
-                <Link href={'/'}><li>Mugs</li></Link>
+            <ul className='flex items-center space-x-4 font-bold md:text-l'>
+                <Link href={'/hoodies'}><li>Hoodies</li></Link>
+                <Link href={'/tshirts'}><li>Tshirts</li></Link>
+                <Link href={'/stickers'}><li>Stickers</li></Link>
+                <Link href={'/mugs'}><li>Mugs</li></Link>
             </ul>
         </div>
         <div className="cart absolute right-0 top-4 mx-5">
-            <CiShoppingCart className='text-xl md:text-3xl'/>
+            <CiShoppingCart className='text-xl md:text-2xl'/>
         </div>
     </div>
   )
