d949b3c821faaa6e576a0e67889fd56e9a2522ce
SanketShendge21 <sanketshendge21052002@gmail.com>
2023-12-27 17:05:28 +0530

    Changed some routing links

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -56,7 +56,7 @@ const Navbar = ({cart, addToCart, removeFromCart, clearCart, subTotal, user, key
 				<span onMouseOver={()=>{setDropdown(true)}} onMouseLeave={()=>{setDropdown(false)}}>
 				{ dropdown && <div className="absolute right-8 top-6 py-4 bg-white shadow-lg border rounded-md px-5 w-32">
 					<ul>
-						<Link href={'/orders'}><li className="py-1 hover:text-orange-400 cursor-pointer text-sm font-bold">Orders</li></Link>
+						<Link href={'/myorders'}><li className="py-1 hover:text-orange-400 cursor-pointer text-sm font-bold">Orders</li></Link>
 						<Link href={'/myaccount'}><li className="py-1 hover:text-orange-400 cursor-pointer text-sm font-bold">My Account</li></Link>
 						<li onClick={logout} className="py-1 hover:text-orange-400 cursor-pointer text-sm font-bold">Logout</li>
 					</ul>
