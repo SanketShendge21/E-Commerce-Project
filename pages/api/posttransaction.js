4d3200fed99dc6e8c71566a7198ff9b81faef6f7
SanketShendge21 <sanketshendge21052002@gmail.com>
2023-12-28 20:01:41 +0530

    Implemented Paytm Payment Gateway still have a TODO

---
+++ pages/api/posttransaction.js
@@ -0,0 +1,10 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+
+  // Update status into orders table after checking the transaction status
+  // Initiate Shipping
+  // Redirect to the order confirmation page
+    res.status(200).json({ body: req.body });
+  }
+  
