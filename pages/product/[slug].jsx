b3a0c3f201c46a433fcf63749d32f2f90b5370c9
SanketShendge21 <sanketshendge21052002@gmail.com>
2023-12-30 20:59:18 +0530

    Changed static text to dynamic

--- pages/product/[slug].jsx
+++ pages/product/[slug].jsx
@@ -200,7 +200,7 @@ export default function Slug({ addToCart, product, variants, buyNow }) {
 								{/* Adding the item to the cart */}
 								<button
 									onClick={() => {
-										addToCart(slug, 1, 499, product.title, size, color);
+										addToCart(slug, product.qty, product.price, product.title, size, color);
 									}}
 									className="flex ml-4 text-white bg-orange-500 border-0 py-2 px-2 md:px-6 focus:outline-none hover:bg-orange-600 rounded"
 								>
