a162d6a711710e9bfcbb0d0fc7631b0fa9f3f17c
SanketShendge21 <sanketshendge21052002@gmail.com>
2023-12-30 20:58:57 +0530

    Added state vars and checkout form

--- pages/checkout.jsx
+++ pages/checkout.jsx
@@ -1,63 +1,89 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { FaCircleMinus, FaCirclePlus } from "react-icons/fa6";
 import { CiMoneyCheck1 } from "react-icons/ci";
 import Head from "next/head";
 import Script from "next/script";
 
 const Checkout = ({ cart, addToCart, removeFromCart, subTotal }) => {
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [phone, setPhone] = useState("");
+	const [address, setAddress] = useState("");
+	const [pincode, setPincode] = useState("");	
+	const [city, setCity] = useState("");
+	const [state, setState] = useState("");
+	const [disabled, setDisabled] = useState(true);
+
+	const handleChange = (e) => {
+		if (e.target.name === "name") {
+			setName(e.target.value);
+		} else if (e.target.name === "email") {
+			setEmail(e.target.value);
+		} else if (e.target.name === "phone") {
+			setPhone(e.target.value);
+		} else if (e.target.name === "address") {
+			setAddress(e.target.value);
+		} else if (e.target.name === "pincode") {
+			setPincode(e.target.value);
+		}
+		if (name.length > 3 && email.length > 3 && phone.length > 3 && address.length > 3 && pincode.length > 3) {
+			setDisabled(false);
+		} else {
+			setDisabled(true);
+		}
+	};
+
 	// Payment Function
 	const initiatePayment = async () => {
-    let oid = Math.floor(Math.random() * Date.now());
-    
-    //Get a transaction token
-	let url = `${process.env.NEXT_PUBLIC_HOST}/api/pretransaction`;
-    const data = {cart, subTotal, oid, email:"email"}
-    const response = await fetch(url, {
-      method: "POST", // *GET, POST, PUT, DELETE, etc.
-      headers: {
-        "Content-Type": "application/json",
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: JSON.stringify(data), // body data type must match "Content-Type" header
-    });
-    let txnRes = await response.json(); // parses JSON response into native JavaScript objects
-    let txnToken = txnRes.txnToken;
-	console.log(txnToken);
-	console.log(txnRes);
-  
+		let oid = Math.floor(Math.random() * Date.now());
 
-    var config = {
-      flow:"DEFAULT",
-      //Optional to hide paymode label when only one paymode is available
-      hidePaymodeLabel: true,
-      data:{
-        orderId:oid,
-        amount: subTotal,
-        token:txnToken,
-        tokenType:"TXN_TOKEN"
-      },
-      style: {
-        //Optional: global style that will apply to all paymodes
-        bodyColor: "green"
-      },
-      merchant:{
-        mid:process.env.NEXT_PUBLIC_PAYTM_MID
-      },
-      handler: {
-        notifyMerchant: function(eventType, data) {
-            console.log("notify merchant called", eventType, data);
-        }
-      }
-     };
-        window.Paytm.CheckoutJS.init(config).then(function onSuccess(){
-          // after successfully updating configuration, invoke JS Checkout
-		  console.log("initialized");
-          window.Paytm.CheckoutJS.invoke();
-        }).catch(function onError(error){
-          console.log("error => ", error);
-        });
-	}
+		//Get a transaction token
+		let url = `${process.env.NEXT_PUBLIC_HOST}/api/pretransaction`;
+		const data = { cart, subTotal, oid, email, name, address, pincode, phone };
+		const response = await fetch(url, {
+			method: "POST", // *GET, POST, PUT, DELETE, etc.
+			headers: {
+				"Content-Type": "application/json",
+				// 'Content-Type': 'application/x-www-form-urlencoded',
+			},
+			body: JSON.stringify(data), // body data type must match "Content-Type" header
+		});
+		let txnRes = await response.json(); // parses JSON response into native JavaScript objects
+		let txnToken = txnRes.txnToken;
+
+		var config = {
+			flow: "DEFAULT",
+			//Optional to hide paymode label when only one paymode is available
+			hidePaymodeLabel: true,
+			data: {
+				orderId: oid,
+				amount: subTotal,
+				token: txnToken,
+				tokenType: "TXN_TOKEN",
+			},
+			style: {
+				//Optional: global style that will apply to all paymodes
+				bodyColor: "green",
+			},
+			merchant: {
+				mid: process.env.NEXT_PUBLIC_PAYTM_MID,
+			},
+			handler: {
+				notifyMerchant: function (eventType, data) {
+					console.log("notify merchant called", eventType, data);
+				},
+			},
+		};
+		window.Paytm.CheckoutJS.init(config)
+			.then(function onSuccess() {
+				// after successfully updating configuration, invoke JS Checkout
+				window.Paytm.CheckoutJS.invoke();
+			})
+			.catch(function onError(error) {
+				console.log("error => ", error);
+			});
+	};
 
 	return (
 		<div className="container px-8 sm:m-auto">
@@ -67,8 +93,8 @@ const Checkout = ({ cart, addToCart, removeFromCart, subTotal }) => {
 			<Script
 				type="application/javascript"
 				crossOrigin="anonymous"
-				src={`${process.env.NEXT_PUBLIC_PAYTM_HOST}/merchantpgpui/checkoutjs/merchants/${process.env.NEXT_PUBLIC_PAYTM_MID}.js`}>
-			</Script>
+				src={`${process.env.NEXT_PUBLIC_PAYTM_HOST}/merchantpgpui/checkoutjs/merchants/${process.env.NEXT_PUBLIC_PAYTM_MID}.js`}
+			></Script>
 			<h1 className="font-bold text-xl my-8 text-center">Checkout</h1>
 			<h2 className="text-xl font-bold">Delivery Details</h2>
 			<div className="mx-auto flex my-2">
@@ -78,6 +104,8 @@ const Checkout = ({ cart, addToCart, removeFromCart, subTotal }) => {
 							Name
 						</label>
 						<input
+							onChange={handleChange}
+							value={name}
 							type="text"
 							id="name"
 							name="name"
@@ -92,6 +120,8 @@ const Checkout = ({ cart, addToCart, removeFromCart, subTotal }) => {
 							Email
 						</label>
 						<input
+							onChange={handleChange}
+							value={email}
 							type="email"
 							id="email"
 							name="email"
@@ -107,6 +137,8 @@ const Checkout = ({ cart, addToCart, removeFromCart, subTotal }) => {
 						Address
 					</label>
 					<textarea
+						onChange={handleChange}
+						value={address}
 						id="address"
 						name="address"
 						className="w-full bg-white rounded border border-gray-300 focus:border-orange-500 focus:ring-2 focus:ring-orange-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
@@ -123,6 +155,8 @@ const Checkout = ({ cart, addToCart, removeFromCart, subTotal }) => {
 							Phone
 						</label>
 						<input
+							onChange={handleChange}
+							value={phone}
 							type="phone"
 							id="phone"
 							name="phone"
@@ -133,13 +167,15 @@ const Checkout = ({ cart, addToCart, removeFromCart, subTotal }) => {
 
 				<div className="px-2 w-1/2">
 					<div className="mb-4">
-						<label htmlFor="city" className="leading-7 text-sm text-gray-600">
-							City
+						<label htmlFor="pincode" className="leading-7 text-sm text-gray-600">
+							Pincode
 						</label>
 						<input
+							onChange={handleChange}
+							value={pincode}
 							type="text"
-							id="city"
-							name="city"
+							id="pincode"
+							name="pincode"
 							className="w-full bg-white rounded border border-gray-300 focus:border-orange-500 focus:ring-2 focus:ring-orange-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
 						/>
 					</div>
@@ -152,24 +188,28 @@ const Checkout = ({ cart, addToCart, removeFromCart, subTotal }) => {
 							State
 						</label>
 						<input
+						value={state}
 							type="text"
 							id="state"
 							name="state"
 							className="w-full bg-white rounded border border-gray-300 focus:border-orange-500 focus:ring-2 focus:ring-orange-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+							readOnly={true}
 						/>
 					</div>
 				</div>
 
 				<div className="px-2 w-1/2">
 					<div className="mb-4">
-						<label htmlFor="pincode" className="leading-7 text-sm text-gray-600">
-							Pincode
+						<label htmlFor="city" className="leading-7 text-sm text-gray-600">
+							City
 						</label>
 						<input
+						value={city}
 							type="text"
-							id="pincode"
-							name="pincode"
+							id="city"
+							name="city"
 							className="w-full bg-white rounded border border-gray-300 focus:border-orange-500 focus:ring-2 focus:ring-orange-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+							readOnly={true}
 						/>
 					</div>
 				</div>
@@ -213,8 +253,9 @@ const Checkout = ({ cart, addToCart, removeFromCart, subTotal }) => {
 			<div className="mx-4">
 				<Link href={"/checkout"}>
 					<button
+						disabled={disabled}
 						onClick={initiatePayment}
-						className="flex mr-2 text-white bg-orange-500 border-0 py-2 pr-2 focus:outline-none hover:bg-orange-600 rounded text-sm"
+						className="flex mr-2 text-white disabled:bg-orange-300 bg-orange-500 border-0 py-2 pr-2 focus:outline-none hover:bg-orange-600 rounded text-sm"
 					>
 						<CiMoneyCheck1 className="m-1 text-center text-xl" /> <span className="mt-1 font-semibold">Pay : ₹{subTotal}</span>
 					</button>
