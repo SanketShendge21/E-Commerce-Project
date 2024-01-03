8e9300e88f3a4c1df4dcd23db48f1012396c6d22
SanketShendge21 <sanketshendge21052002@gmail.com>
2024-01-03 16:25:35 +0530

    Modify the available quantity when a product is purchased

--- pages/api/posttransaction.js
+++ pages/api/posttransaction.js
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import connectDB from "@/middleware/mongoose";
 import Order from "@/models/Order"
+import Product from "@/models/Product";
 
 const handler = async (req, res)=> {
   // Validate Paytm Checksum -- TODO
   // Update status into orders table after checking the transaction status
-  let order;
+  let order,products;
   if(req.body.STATUS == 'TXN_SUCCESS'){
     order = await Order.findOneAndUpdate({orderId: req.body.ORDERID}, {status : 'Paid', paymentInfo: JSON.stringify(req.body)});
+    products = order.products
+    // Iterate over purchased products to update status of quantity
+    for(let slug in products){
+      await Product.findOneAndUpdate({slug: slug},{$inc :{"availableQty": - products[slug].qty}})
+    }
   }
   else if(req.body.STATUS == 'PENDING'){
     order = await Order.findOneAndUpdate({orderId: req.body.ORDERID}, {status : 'Pending', paymentInfo: JSON.stringify(req.body)});
@@ -15,7 +21,7 @@ const handler = async (req, res)=> {
   // Initiate Shipping
   // Redirect to the order confirmation page
 
-    res.redirect('/order?id=?'+order._id,200);
+    res.redirect('/order?id=' + order._id + '&clearCart=1',200);
     // res.status(200).json({ body: req.body });
   }
   
