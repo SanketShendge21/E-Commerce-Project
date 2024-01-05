cc1c9904954240d785ff4a8bb4f890a6d19a86eb
SanketShendge21 <sanketshendge21052002@gmail.com>
2024-01-05 18:28:42 +0530

    Fetch pincodes from json file

--- pages/api/pincode.js
+++ pages/api/pincode.js
@@ -1,13 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import pincodes from '@/pincodes.json'
+
 export default function handler(req, res) {
     // Pincode object corresponding to state and cities
-    let pincodes ={
-      "411048": ["Pune","Maharastra"],
-      "721302": ["Kharagpur","West Bengal"],
-      "110003": ["Delhi", "Delhi"],
-      "560017" : ["Banglore", "Karnataka"]
-    }
+    // let pincodes ={
+    //   "411048": ["Pune","Maharastra"],
+    //   "721302": ["Kharagpur","West Bengal"],
+    //   "110003": ["Delhi", "Delhi"],
+    //   "560017" : ["Banglore", "Karnataka"]
+    // }
     res.status(200).json(pincodes)
   }
   
