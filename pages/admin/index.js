c1a5850b312c687c500f44525c1f69b07a8222df
SanketShendge21 <sanketshendge21052002@gmail.com>
2024-01-11 13:12:47 +0530

    If login then dashboard otherwise login

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Sidebar from './sidebar'
+import { useRouter } from 'next/router'
 const Index = () => {
+  const router = useRouter()
+  useEffect(() => {
+    if(!localStorage.getItem('admin')){
+      router.push('/admin/login')
+    }
+  }, [])
   return (
     <div className='min-h-screen'>
+
       <Sidebar></Sidebar>
     </div>
   )
