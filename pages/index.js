d01c2324fb664d70c374b29eac42e6cba51e8961
Sanket Shendge <105985549+SanketShendge21@users.noreply.github.com>
2025-01-05 19:44:04 +0530

    Added affiliate products

--- pages/index.js
+++ pages/index.js
@@ -30,6 +30,50 @@ const Home = ({tshirts, hoodies,addToCart}) => {
 
         <DynamicCarousel />
 		<section className="container mx-auto my-8">
+    <h2 className="text-3xl font-bold mb-4">From ThreadsUnveiled</h2>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+    
+		
+      <div key="101" className="bg-white p-4 rounded-md shadow-2xl flex flex-col transition-transform transform hover:scale-105">
+        <div className="mb-4 relative overflow-hidden rounded-md">
+          <div className=" cursor-pointer absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-0 hover:opacity-75 transition-opacity duration-300"></div>
+        <img src={`https://m.media-amazon.com/images/I/71OFKtclW4L._AC_AA180_.jpg`} alt={`Not found`} className=" max-w-full max-h-full object-cover rounded-md hover:opacity-75 transition-opacity duration-300" />
+        </div>
+        <h3 className="text-xl font-semibold text-gray-800">GIGABYTE NVIDIA GeForce RTX 3060 WINDFORCE OC 12GB GDDR6 pci_e_x16 Graphics Card (GV-N3060WF2OC-12GD)</h3>
+        <Link target="_blank" href={`https://amzn.to/3DJnjuu`}><button className="flex  ml-4 text-white bg-orange-500 border-0 disabled:bg-orange-300 py-2 px-2 md:px-6 focus:outline-none hover:bg-orange-600 rounded"
+								>
+									<BsFillCartPlusFill className="mr-1 mt-1 text-center" />
+									Buy Now
+								</button></Link>
+      </div>
+
+      <div key="102" className="bg-white p-4 rounded-md shadow-2xl flex flex-col transition-transform transform hover:scale-105">
+        <div className="mb-4 relative overflow-hidden rounded-md">
+          <div className=" cursor-pointer absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-0 hover:opacity-75 transition-opacity duration-300"></div>
+        <img src={`https://m.media-amazon.com/images/I/51nnZ+cziYL._AC_AA180_.jpg`} alt={`Not found`} className=" max-w-full max-h-full object-cover rounded-md hover:opacity-75 transition-opacity duration-300" />
+        </div>
+        <h3 className="text-xl font-semibold text-gray-800">Crucial P3 1TB PCIe 3.0 3D NAND NVMe M.2 SSD, up to 3500MB/s - CT1000P3SSD8</h3>
+        <Link target="_blank" href={`https://amzn.to/3BWWSAX`}><button className="flex  ml-4 text-white bg-orange-500 border-0 disabled:bg-orange-300 py-2 px-2 md:px-6 focus:outline-none hover:bg-orange-600 rounded"
+								>
+									<BsFillCartPlusFill className="mr-1 mt-1 text-center" />
+									Buy Now
+								</button></Link>
+      </div> 
+
+      <div key="103" className="bg-white p-4 rounded-md shadow-2xl flex flex-col transition-transform transform hover:scale-105">
+        <div className="mb-4 relative overflow-hidden rounded-md">
+          <div className=" cursor-pointer absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-0 hover:opacity-75 transition-opacity duration-300"></div>
+        <img src={`https://m.media-amazon.com/images/I/41ETl9FwP8L._AC_AA180_.jpg`} alt={`Not found`} className=" max-w-full max-h-full object-cover rounded-md hover:opacity-75 transition-opacity duration-300" />
+        </div>
+        <h3 className="text-xl font-semibold text-gray-800">TP-Link Archer T2Ub Nano Ac600 USB 2.0 Wi-Fi Bluetooth 4.2 Desktop USB Adapter|Dual Band Wireless 600 Mbps|Plug and Play|Supports Windows 11/10/8.1/8/7 for WiFi,Windows 11/10/8.1/7 for Bluetooth</h3>
+        <Link target="_blank" href={`https://amzn.to/4h4WD5Z`}><button className="flex  ml-4 text-white bg-orange-500 border-0 disabled:bg-orange-300 py-2 px-2 md:px-6 focus:outline-none hover:bg-orange-600 rounded"
+								>
+									<BsFillCartPlusFill className="mr-1 mt-1 text-center" />
+									Buy Now
+								</button></Link>
+      </div>
+  </div>
+  <br></br>
   <h2 className="text-3xl font-bold mb-4">Featured T-shirts</h2>
   <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
     {tshirts.map((tshirt) => (
